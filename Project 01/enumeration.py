a74990ffe4aaaec253706ffb51354bcd98f2d568
pdchang <changp@oregonstate.edu>
2017-04-19 22:11:55 -0400

    Add files via upload

--- Project 01/enumeration.py
+++ Project 01/enumeration.py
@@ -1,4 +1,7 @@
 #Philip Chang - CS 325 - Project 01 part 1
+import sys
+import re
+
 def enumeration(array):
     if len(array) == 0:
         return 0
@@ -12,6 +15,27 @@ def enumeration(array):
                 temp += array[k]
             if temp > max_sum:
                 max_sum = temp
-                mindx = i
-                maxdx = j
-    return (array[mindx:(maxdx + 1)], max_sum)
+                sub = array[i:j+1]
+    return [sub, max_sum]
+
+
+def mss():
+
+    inputfile = open('MSS_Problems.txt')
+    outputfile = open('MSS_Results.txt', 'a')
+
+    for line in inputfile:
+        array = map(int, line.strip().split(' '))
+        answer = enumeration(array)
+        outputfile.write(str(array).replace('[','').replace(']', '') + '\n')
+        outputfile.write(str(answer[0]).replace('[','').replace(']', '') + '\n')
+        outputfile.write(str(answer[1]).replace('[','').replace(']', '') + '\n\n')
+
+    inputfile.close()
+    outputfile.close()
+
+
+    inputfile.close()
+    outputfile.close()
+
+mss()
